aeaffc33f38dd1e0e9775a377c709775cae907d2
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-10-20 14:21:05 +0900

    trainデータとtestデータを分割

--- create_raw_dataset.py
+++ create_raw_dataset.py
@@ -9,6 +9,7 @@ import numpy as np
 from skimage import io
 import tqdm
 import h5py
+from fuel.datasets.hdf5 import H5PYDataset
 
 
 if __name__ == '__main__':
@@ -16,16 +17,18 @@ if __name__ == '__main__':
     channel = 3
     height = 56
     width = 56
+    test_size = 20000
 
     image_list = []
     images = []
-    f = open(r"hdd\random_file_path.txt", "r")
+    f = open(r"C:\Users\yamane\Dropbox\colorization\random_file_path.txt", "r")
     for path in f:
         path = path.strip()
         image_list.append(path)
     f.close()
 
     num_images = len(image_list)
+    num_train = num_images - test_size
 
     f = h5py.File(r'E:\raw_resized_dataset_random_save_56\raw_dataset_56.hdf5',
                   mode='w')
@@ -48,5 +51,10 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print "割り込み停止が実行されました"
 
+    split_dict = {
+            'train': {'image_features': (0, num_train)},
+            'test': {'image_features': (num_train, num_images)}}
+
+    f.attrs['split'] = H5PYDataset.create_split_array(split_dict)
     f.flush()
     f.close()
